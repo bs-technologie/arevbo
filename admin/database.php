2832cb5fb88191d7cf937d3c5c4b6741e897c2ef
bs-technologie <87404475+bs-technologie@users.noreply.github.com>
2021-07-14 03:33:54 +0000

    Delete admin directory

--- admin/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try {
-    $db = new PDO('mysql:host=localhost;port=3306;dbname=arevbo', 'root', '');
-    $db-> SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-}catch(PDOException $e){
-    echo $e->getMessage();
-}
-?>
